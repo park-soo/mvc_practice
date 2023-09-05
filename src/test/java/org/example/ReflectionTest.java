311bb505376dbe3b049bf1e5888f3ee7f25fbc96
parkmansoo <parkman.xxx@gmail.com>
2023-09-05 23:27:53 +0900

    • @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.

---
+++ src/test/java/org/example/ReflectionTest.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import org.example.annotation.Controller;
+import org.example.annotation.Service;
+import org.example.model.User;
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+    @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
+*/
+public class ReflectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+    @Test
+    void controllerScan() {
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+
+        logger.debug("beans: [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+
+        logger.debug("User all declared fields [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        logger.debug("User all declared constructors [{}]", Arrays.stream(clazz.getConstructors()).collect(Collectors.toList()));
+        logger.debug("User all declared method [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        //힙 영역에 로드돼 있는 클래스 타입의 객체를  가져오는 3가지 방법
+        // 1
+        Class<User> clazz = User.class;
+
+        // 2
+        User user = new User("serverWizard", "아무개");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz);
+        logger.debug("clazz2: [{}]", clazz2);
+        logger.debug("clazz3: [{}]", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
+    }
+
+    private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+//        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+//        beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        return beans;
+    }
+}
